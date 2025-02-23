e08deb820d5c8d78cfe2522c9e19da1d71cee358
Lucas <lucasrech00@gmail.com>
2025-02-23 10:41:00 -0300

    first commit

---
+++ frontend/app/components/card.tsx
@@ -0,0 +1,7 @@
+export default function Card() {
+    return (
+        <div className="bg-red-500 w-1/4 h-4/5 rounded-xl">
+            <h1>hello</h1>
+        </div>
+    )
+}
---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Card from "./components/card";
+
+export default function Home() {
+  return (
+    <div className=" flex flex-col justify-center items-center w-full h-screen bg-gradient-to-t from-[#05050e] via-[#002446] to-[#00ffc0]">
+        <Card />
+    </div>
+  );
+}
