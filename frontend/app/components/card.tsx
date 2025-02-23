72cd4be105b27702d4176b8cd4584b713e149983
Lucas <lucasrech00@gmail.com>
2025-02-23 11:03:53 -0300

    componente: aplicando responsividade ao card principal

--- frontend/app/components/card.tsx
+++ frontend/app/components/card.tsx
@@ -1,6 +1,12 @@
 export default function Card() {
     return (
-        <div className="bg-red-500 w-1/4 h-4/5 rounded-xl">
+        <div className="
+            bg-white 
+            w-[300] h-3/5
+            md:w-[350] md:h-4/5
+            lg:w-[450] lg:h-4/5  
+            rounded-xl"
+        >
             <h1>hello</h1>
         </div>
     )
