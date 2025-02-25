76bd447e4b9d0873c78b30dca69a53af8d961419
Lucas <lucasrech00@gmail.com>
2025-02-25 00:05:21 -0300

    componente: criado array de imagens e alterado nome de componente principal

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,9 @@
-import Card from "./components/card";
+import WeatherCard from "./components/weatherCard";
 
 export default function Home() {
   return (
     <div className=" flex flex-col justify-center items-center w-full h-screen bg-gradient-to-t from-[#05050e] via-[#002446] to-[rgb(88,0,110)]">
-        <Card />
+        <WeatherCard />
     </div>
   );
 }
