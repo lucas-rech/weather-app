2ce1e91ee85b82304ad036794ba46adea7804d1a
Lucas <lucasrech00@gmail.com>
2025-02-24 23:26:17 -0300

    componente: adicionado hover UX no botão de buscar

--- frontend/app/components/searchBar.tsx
+++ frontend/app/components/searchBar.tsx
@@ -3,7 +3,7 @@ export default function SearchBar() {
         <div className="mt-6 flex justify-center align-center w-full">
             <form action="#" className="flex flex-row justify-between gap-2 h-[35px] w-[100%] mr-8 ml-8">
                 <input type="text" placeholder="Digite uma cidade" className="bg-white bg-opacity-70  rounded-xl pl-4 w-[60%]"/>
-                <button type="submit" className="bg-[rgb(88,0,110)] text-white w-[30%] rounded-xl">Buscar</button>
+                <button type="submit" className="bg-[#390e44] text-white w-[30%] rounded-xl hover:w-[32%] hover:h-[3px] hover:bg-[#58006e]">Buscar</button>
             </form>
         </div>
     )
